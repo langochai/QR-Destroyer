61bdf5ccbbd12524ffe920e585240da61efe4ec6
La Ngoc Hai <langochai11121998@gmail.com>
2025-03-14 16:05:19 +0700

    uncomment db related code

--- wpf-in-winforms/FrmMain.cs
+++ wpf-in-winforms/FrmMain.cs
@@ -18,51 +18,50 @@ namespace wpf_in_winforms
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            GameFrame frm = new GameFrame();
-            //frm.customer = newCustomer;
-            frm.FormClosed += (s, ev) => {
-                frm.scannerSerialPort.Close();
-                frm.scannerSerialPort.Dispose();
-                frm.scannerSerialPort = null;
-                this.Show(); 
-            };
-            this.Hide();
-            frm.Show();
-            //if (ValidateInputs())
-            //{
-            //    var InterestIDs = new List<int>();
-            //    if(chkInterest1.Checked) InterestIDs.Add(1);
-            //    if(chkInterest2.Checked) InterestIDs.Add(2);
-            //    if(chkInterest3.Checked) InterestIDs.Add(3);
-            //    if(chkInterest4.Checked) InterestIDs.Add(4);
-            //    if(chkInterest5.Checked) InterestIDs.Add(5);
-            //    if(chkInterest6.Checked) InterestIDs.Add(6);
-            //    if(chkInterest7.Checked) InterestIDs.Add(7);
-            //    var ChannelIDs = new List<int>();
-            //    if (chkChannel1.Checked) ChannelIDs.Add(1);
-            //    if (chkChannel2.Checked) ChannelIDs.Add(2);
-            //    if (chkChannel3.Checked) ChannelIDs.Add(3);
-            //    if (chkChannel4.Checked) ChannelIDs.Add(4);
-            //    if (chkChannel5.Checked) ChannelIDs.Add(5);
+            if (ValidateInputs())
+            {
+                var InterestIDs = new List<int>();
+                if (chkInterest1.Checked) InterestIDs.Add(1);
+                if (chkInterest2.Checked) InterestIDs.Add(2);
+                if (chkInterest3.Checked) InterestIDs.Add(3);
+                if (chkInterest4.Checked) InterestIDs.Add(4);
+                if (chkInterest5.Checked) InterestIDs.Add(5);
+                if (chkInterest6.Checked) InterestIDs.Add(6);
+                if (chkInterest7.Checked) InterestIDs.Add(7);
+                var ChannelIDs = new List<int>();
+                if (chkChannel1.Checked) ChannelIDs.Add(1);
+                if (chkChannel2.Checked) ChannelIDs.Add(2);
+                if (chkChannel3.Checked) ChannelIDs.Add(3);
+                if (chkChannel4.Checked) ChannelIDs.Add(4);
+                if (chkChannel5.Checked) ChannelIDs.Add(5);
 
-            //    var newCustomer = new Customers
-            //    {
-            //        Name = txtName.Text,
-            //        Company = txtCompany.Text,
-            //        Email = txtEmail.Text,
-            //        PhoneNumber = txtPhoneNumber.Text,
-            //        PlayTime = 0,
-            //        InterestIds = JsonConvert.SerializeObject(InterestIDs),
-            //        ChannelIds = JsonConvert.SerializeObject(ChannelIDs),
-            //        CreatedDate = DateTime.Now,
-            //    };
-            //    SqliteHelper<Customers>.Insert(newCustomer);
-
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            //}
+                var newCustomer = new Customers
+                {
+                    Name = txtName.Text,
+                    Company = txtCompany.Text,
+                    Email = txtEmail.Text,
+                    PhoneNumber = txtPhoneNumber.Text,
+                    PlayTime = 0,
+                    InterestIds = JsonConvert.SerializeObject(InterestIDs),
+                    ChannelIds = JsonConvert.SerializeObject(ChannelIDs),
+                    CreatedDate = DateTime.Now,
+                };
+                SqliteHelper<Customers>.Insert(newCustomer);
+                GameFrame frm = new GameFrame();
+                frm.customer = newCustomer;
+                frm.FormClosed += (s, ev) => {
+                    frm.scannerSerialPort.Close();
+                    frm.scannerSerialPort.Dispose();
+                    frm.scannerSerialPort = null;
+                    this.Show();
+                };
+                this.Hide();
+                frm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void DisplayPixelFont()
